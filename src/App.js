b9380bcec98aefa2a0e859c7134a0fbed4831c06
Jayanithi-R <jayanithi.2253023@srec.ac.in>
2025-08-18 15:12:59 +0530

    worked on how it works section

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import Navbar from './components/Navbar';
-
+import HeroSection from './components/HeroSection';
+import HowItWorks from './components/HowItWorks';
 function App() {
   return (
     <div>
       <Navbar />
+      <HeroSection />
+      <HowItWorks />
     </div>
   );
 }
