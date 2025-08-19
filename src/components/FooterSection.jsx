ad3016d8f4b7fe6cdf8fc8e081c8ed2437c27456
Jayanithi-R <jayanithi.2253023@srec.ac.in>
2025-08-19 16:53:13 +0530

    working on FooterSection.jsx

---
+++ src/components/FooterSection.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import rainbow from "./assets/rainbow.png";
+
+const styles = {
+    container: {
+        backgroundColor:"#f7f6f3",
+        padding:"60px 20px",
+        fontFamily:"'Inter', sans-serif",
+        display: "flex",
+        justifyContent: "center",
+    },
+
+    footerBox: {
+        display: "flex",
+        justifyContent: "space-between",
+        alignItems:"strech",
+        maxWidth:"1000px",
+        width:"100%",
+        gap: "40px",
+        flexWrap: "wrap",
+    },
+
+    left: {
+        background: "#fff",
+        borderRadius:"20px",
+        padding: "30px",
+        flex: 1,
+        minWidth: "280px",
+    },
+
+    brand: {
+        
+    }
+}
